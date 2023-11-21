27e7a125520ce897c0096f448b75d73701726bd1
John Miller <john.ifert.miller@gmail.com>
2023-11-21 17:04:16 -0500

    template set

---
+++ src/components/Footer.js
@@ -0,0 +1,37 @@
+function Footer() {
+    return (       
+        <footer className="text-center text-lg-start bg-white text-muted">  
+          <section className="">
+            <div className="container text-center text-md-start mt-5">          
+              <div className="row mt-3">             
+                <div className="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">              
+                  <h6 className="text-uppercase fw-bold mb-4">
+                    <i className="fas fa-gem me-3 text-secondary"></i>Wild Bergamot
+                  </h6>
+                  <p>
+                  Dedicated to giving you the best experience,
+                  request a quote today!
+                  </p>
+                </div>
+                <div className="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+                  <h6 className="text-uppercase fw-bold mb-4">Info</h6>
+                  <p><i className="fas fa-home me-3 text-secondary"></i> Marshall, NC 28753, US</p>
+                  <p>
+                    <i className="fas fa-envelope me-3 text-secondary"></i>
+                    wild.bergmot.gardening@gmail.com
+                  </p>
+                  {/* <p><i className="fas fa-clock me-3 text-secondary"></i>Mon-Fri | 8am-4pm</p> */}
+                  {/* <p><i className="fas fa-print me-3 text-secondary"></i> + 01 234 567 89</p> */}
+                </div>      
+              </div>        
+            </div>
+          </section>
+          {/* <div className="text-center p-4" style={{backgroundColor: "rgba(0, 0, 0, 0.025)"}}>
+            © 2021 Copyright:
+            <a className="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+          </div> */}
+        </footer>
+    )
+}
+
+export default Footer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import Container from 'react-bootstrap/Container';
+import { Navbar, Nav } from 'react-bootstrap';
+
+function NavbarComponent() {
+  const navbarStyle = {
+    backgroundColor: "#606c38",
+    position: 'fixed',
+    height: '110px',
+    width: '100%',
+    zIndex: '1',
+  };
+
+  const lineStyle = {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    height: '20px',
+    backgroundColor: "#283618",
+  };
+
+  const whiteTextStyle = {
+    color: '#fefae0',
+    marginTop: '25px', // Add margin to push the text down
+    fontFamily: 'Dancing Script, cursive',
+    fontSize: '20px',
+  };
+
+  const lineAboveNavLinksStyle = {
+    position: 'absolute',
+    top: '100px', // Position the line below the Navbar
+    left: 0,
+    right: 0,
+    backgroundColor: "#283618",
+  };
+
+  return (
+    <div style={navbarStyle}>
+      <div style={lineStyle}></div>
+      <Navbar expand="lg">
+        <Container fluid>
+          <Navbar.Brand style={{ ...whiteTextStyle, fontSize: '40px' }}>Wild Bergamot</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className='navDropdown' style={{ backgroundColor: "#606c38" }}>
+              <Nav.Link href="/landscapingwildbergamot" className="button-pop-out" style={{ ...whiteTextStyle, backgroundColor: "#606c38"}}>Home</Nav.Link>
+              <Nav.Link href="#services" className="button-pop-out" style={{ ...whiteTextStyle, backgroundColor: "#606c38"}}>Services</Nav.Link>
+              <Nav.Link href="#about" className="button-pop-out" style={{ ...whiteTextStyle, backgroundColor: "#606c38"}}>About</Nav.Link>           
+            </Nav> 
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+      <div style={lineAboveNavLinksStyle}></div>
+    </div>
+  );
+}
+
+export default NavbarComponent;
